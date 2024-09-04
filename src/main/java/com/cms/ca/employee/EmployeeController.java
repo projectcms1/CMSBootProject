e4e48d37db667698c1cad1f4a5a1f7ed9aec5138
clcc0 <clcc0@LAPTOP-GSGAHP2C>
2024-09-04 18:40:51 +0900

    employee header, footer, info: finished employee counsel_index: ~ing

--- src/main/java/com/cms/ca/employee/EmployeeController.java
+++ src/main/java/com/cms/ca/employee/EmployeeController.java
@@ -1,8 +1,33 @@
 package com.cms.ca.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cms.ca.test_repo;
 
 @Controller
 public class EmployeeController {
+	@Autowired
+	test_repo repo;
 
+	@GetMapping("/")
+	public String portalMain() {
+		return "default-header";
+	}
+	
+	@GetMapping("/login")
+	public String loginPage() {
+		return "login";
+	}
+	
+	@GetMapping("/empy_info")
+	public String employeeInfoPage() {
+		return "employee/empy_info";
+	}
+	
+	@GetMapping("/empy_counsel_index")
+	public String employeeCounselingIndexPage() {
+		return "employee/empy_counsel_index";
+	}
 }
