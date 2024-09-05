9ee04ea7be2545cacd7925bdda1a630a93501899
sej <sej@localhost>
2024-09-05 14:56:08 +0900

    240905 관리자 프론트v1

--- src/main/java/com/cms/ca/admin/AdminController.java
+++ src/main/java/com/cms/ca/admin/AdminController.java
@@ -1,8 +1,45 @@
 package com.cms.ca.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+	
+	@GetMapping("/login")
+	public String admin_login() {
+		return "admin/login";
+	}
+	
+	@GetMapping("/adminfindid")
+	public String find_id() {
+		return "admin/adminfindid";
+	}
+	
+	@GetMapping("/")
+	public String stlist_mod() {
+		return "admin/stlistmod";
+	}
+	
+	@GetMapping("/prolistmod")
+	public String prolist_mod() {
+		return "admin/prolistmod";
+	}
+	
+	@GetMapping("/adminlistmod")
+	public String adminlist_mod() {
+		return "admin/adminlistmod";
+	}
+	
+	@GetMapping("/proinfo")
+	public String pro_info() {
+		return "admin/proinfo";
+	}
+	
+	@GetMapping("/ptnoticemod")
+	public String portalnotice_mod() {
+		return "admin/ptnoticemod";
+	}
+
 
 }
