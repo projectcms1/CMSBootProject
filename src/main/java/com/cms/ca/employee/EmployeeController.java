ef78066cb80ce8c0a275634bb5db0fae50df3efb
clcc0 <clcc0@LAPTOP-GSGAHP2C>
2024-09-09 16:23:34 +0900

    교직원_html 완료

--- src/main/java/com/cms/ca/employee/EmployeeController.java
+++ src/main/java/com/cms/ca/employee/EmployeeController.java
@@ -17,7 +17,22 @@ public class EmployeeController {
 	}
 	
 	@GetMapping("/empy_counsel_index")
-	public String employeeCounselingIndexPage() {
+	public String employeeCounselIndexPage() {
 		return "employee/empy_counsel_index";
+	}
+	
+	@GetMapping("/empy_counsel_reserve")
+	public String employeeCounselReservePage() {
+		return "employee/empy_counsel_reserve";
+	}
+	
+	@GetMapping("/empy_counsel_add")
+	public String employeeCounselAddPage() {
+		return "employee/empy_counsel_add";
+	}
+	
+	@GetMapping("/empy_counsel_chatting")
+	public String employeeCounselChattingPage() {
+		return "employee/empy_counsel_chatting";
 	}
 }
