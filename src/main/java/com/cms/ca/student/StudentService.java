d7701381c12a045f529b9903fd7ad539d0ab4a9a
tjdals2597 <tjdals2597@naver.com>
2024-09-13 14:26:51 +0900

    update

--- src/main/java/com/cms/ca/student/StudentService.java
+++ src/main/java/com/cms/ca/student/StudentService.java
@@ -1,8 +1,20 @@
 package com.cms.ca.student;
 
+import java.util.List;
+
+import com.cms.ca.counsel_dto;
+import com.cms.ca.employee_dto;
 import com.cms.ca.student_dto;
 
 public interface StudentService {
 
 	student_dto getOneStudent(String stdnt_no);
+	
+	int addCounselReservation(counsel_dto dto);
+	
+	String getProfessorNumber(String std_no);
+	
+	String getPrfsTimeTable(String std_no);
+	
+	List<employee_dto> getAllCounseler();
 }
