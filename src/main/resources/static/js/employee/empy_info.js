b5f542463a5041fdd6f272f4533e20170f665341
clcc0 <clcc0@LAPTOP-GSGAHP2C>
2024-09-13 14:27:44 +0900

    empy_info 이메일 및 전화번호 입력 및 저장 ok

--- src/main/resources/static/js/employee/empy_info.js
+++ src/main/resources/static/js/employee/empy_info.js
@@ -1,6 +1,8 @@
 function personal_input(){
 	emp_eml_addr.disabled=false;
 	emp_telno.disabled=false;
+	emp_telno.value=emp_telno.value.substring(0,3)+ emp_telno.value.substring(4, 8) + emp_telno.value.substring(9);
+	//010-0000-0000 형식으로 된 전화번호를 숫자만 남기도록 바꿔주는 라인
 	
 	emp_zip.disabled=true;
 	emp_addr.disabled=true;
@@ -12,9 +14,23 @@ function personal_input(){
 	dpstr_nm.disabled=true;
 }
 
-function personal_save(){
-
-	personal_info.submit();
+function personal_save(){ 
+	var regex_email=/^[0-9a-zA-Z]*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]*$/i;	
+	//아이디 부분: 숫자 및 영문자만 가능, 도메인: 숫자, 영문자로 시작하며 중간에 숫자, 영문자 및 -_\.이  들어갈 수 있고, . 이후 영문자가 들어가는 형식 
+	
+	var email=emp_eml_addr.value;
+	var tel=emp_telno.value;
+	
+	if(!regex_email.test(email)){
+		alert("이메일을 형식에 맞게 입력해주세요.");
+	}
+	if(tel.length!=11){
+		alert("전화번호 11자리 숫자를 모두 입력해주세요.");
+	}
+	else{
+		alert("개인정보가 정상적으로 변경되셨습니다.")
+		personal_info.submit();
+	}
 }
 
 function addr_input(){
