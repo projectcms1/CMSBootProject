c2d7b999a716fb4f8e55313a00dfb9101d14caf0
kim-danhee <eksgml22@naver.com>
2024-09-26 18:33:05 +0900

    로그인 계정잠금 기능추가

--- src/main/java/com/cms/ca/service/AccountService.java
+++ src/main/java/com/cms/ca/service/AccountService.java
@@ -8,4 +8,8 @@ public interface AccountService {
     login_dto findAccountByLgnId(String lgn_id);
     employee_dto findEmployeeByAccountId(Integer user_no);
     student_dto findStudentByAccountId(Integer user_no);
+    
+    void lockAccount(String lgn_id);	//로그인 실패 처리
+    String isLocked(String lgn_id);	//계정 잠금 여부 확인
+    String login(String lgn_id, String pswd);	//로그인 처리 메서드 추가
 }
