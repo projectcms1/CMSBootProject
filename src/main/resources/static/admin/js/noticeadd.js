32069256fe9fc693dba7ba2470496012f162edf0
tjdals2597 <tjdals2597@naver.com>
2024-09-29 01:07:11 +0900

    공지사항 등록 페이지 CKEDITOR 결합 (admin/css/notice_editor.css -> 크기 조절 필요)

---
+++ src/main/resources/static/admin/js/noticeadd.js
@@ -0,0 +1,257 @@
+import {
+	ClassicEditor,
+	AccessibilityHelp,
+	Autoformat,
+	AutoLink,
+	Autosave,
+	BlockQuote,
+	Bold,
+	Essentials,
+	FindAndReplace,
+	GeneralHtmlSupport,
+	Heading,
+	HorizontalLine,
+	Indent,
+	IndentBlock,
+	Italic,
+	Link,
+	Paragraph,
+	SelectAll,
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
+	Strikethrough,
+	Style,
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableProperties,
+	TableToolbar,
+	TextTransformation,
+	Underline,
+	Undo
+} from 'ckeditor5';
+
+import translations from 'ckeditor5/translations/ko.js';
+
+let editor;
+
+const editorConfig = {
+	toolbar: {
+		items: [
+			'undo',
+			'redo',
+			'|',
+			'findAndReplace',
+			'selectAll',
+			'|',
+			'heading',
+			'style',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'strikethrough',
+			'|',
+			'specialCharacters',
+			'horizontalLine',
+			'link',
+			'insertTable',
+			'blockQuote',
+			'|',
+			'outdent',
+			'indent',
+			'|',
+			'accessibilityHelp'
+		],
+		shouldNotGroupWhenFull: false
+	},
+	plugins: [
+		AccessibilityHelp,
+		Autoformat,
+		AutoLink,
+		Autosave,
+		BlockQuote,
+		Bold,
+		Essentials,
+		FindAndReplace,
+		GeneralHtmlSupport,
+		Heading,
+		HorizontalLine,
+		Indent,
+		IndentBlock,
+		Italic,
+		Link,
+		Paragraph,
+		SelectAll,
+		SpecialCharacters,
+		SpecialCharactersArrows,
+		SpecialCharactersCurrency,
+		SpecialCharactersEssentials,
+		SpecialCharactersLatin,
+		SpecialCharactersMathematical,
+		SpecialCharactersText,
+		Strikethrough,
+		Style,
+		Table,
+		TableCaption,
+		TableCellProperties,
+		TableColumnResize,
+		TableProperties,
+		TableToolbar,
+		TextTransformation,
+		Underline,
+		Undo
+	],
+	heading: {
+		options: [
+			{
+				model: 'paragraph',
+				title: 'Paragraph',
+				class: 'ck-heading_paragraph'
+			},
+			{
+				model: 'heading1',
+				view: 'h1',
+				title: 'Heading 1',
+				class: 'ck-heading_heading1'
+			},
+			{
+				model: 'heading2',
+				view: 'h2',
+				title: 'Heading 2',
+				class: 'ck-heading_heading2'
+			},
+			{
+				model: 'heading3',
+				view: 'h3',
+				title: 'Heading 3',
+				class: 'ck-heading_heading3'
+			},
+			{
+				model: 'heading4',
+				view: 'h4',
+				title: 'Heading 4',
+				class: 'ck-heading_heading4'
+			},
+			{
+				model: 'heading5',
+				view: 'h5',
+				title: 'Heading 5',
+				class: 'ck-heading_heading5'
+			},
+			{
+				model: 'heading6',
+				view: 'h6',
+				title: 'Heading 6',
+				class: 'ck-heading_heading6'
+			}
+		]
+	},
+	htmlSupport: {
+		allow: [
+			{
+				name: /^.*$/,
+				styles: true,
+				attributes: true,
+				classes: true
+			}
+		]
+	},
+	language: 'ko',
+	link: {
+		addTargetToExternalLinks: true,
+		defaultProtocol: 'https://',
+		decorators: {
+			toggleDownloadable: {
+				mode: 'manual',
+				label: 'Downloadable',
+				attributes: {
+					download: 'file'
+				}
+			}
+		}
+	},
+	placeholder: 'Type or paste your content here!',
+	style: {
+		definitions: [
+			{
+				name: 'Article category',
+				element: 'h3',
+				classes: ['category']
+			},
+			{
+				name: 'Title',
+				element: 'h2',
+				classes: ['document-title']
+			},
+			{
+				name: 'Subtitle',
+				element: 'h3',
+				classes: ['document-subtitle']
+			},
+			{
+				name: 'Info box',
+				element: 'p',
+				classes: ['info-box']
+			},
+			{
+				name: 'Side quote',
+				element: 'blockquote',
+				classes: ['side-quote']
+			},
+			{
+				name: 'Marker',
+				element: 'span',
+				classes: ['marker']
+			},
+			{
+				name: 'Spoiler',
+				element: 'span',
+				classes: ['spoiler']
+			},
+			{
+				name: 'Code (dark)',
+				element: 'pre',
+				classes: ['fancy-code', 'fancy-code-dark']
+			},
+			{
+				name: 'Code (bright)',
+				element: 'pre',
+				classes: ['fancy-code', 'fancy-code-bright']
+			}
+		]
+	},
+	table: {
+		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+	},
+	translations: [translations]
+};
+
+ClassicEditor.create(document.querySelector('#editor'), editorConfig).then(newEditor => {
+				editor = newEditor;
+			}).catch(error => {
+				console.error(error);
+			});
+
+document.querySelector("#add_notice_btn").addEventListener("click", function() {
+	var editorData = editor.getData();
+	if (notice_add_frm.ntc_mttr_ttl.value == "") {
+		alert("공지사항 제목을 입력해주세요.");
+		notice_add_frm.ntc_mttr_ttl.focus();
+	}
+	else if (editorData == "") {
+		alert("공지 내용을 입력해주세요.");
+		document.querySelector(".ck-editor__editable_inline").focus();
+	}
+	else {
+		if (confirm("이대로 등록하시겠습니까?")) {
+			notice_add_frm.submit();
+		}
+	}
+});
